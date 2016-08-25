21c53173c6aba7a53943226acc88e5c9cab5ec3a
wangjh1 <627658543@qq.com>
2016-08-25 17:01:27 +0800

    redis

--- common/config/main.php
+++ common/config/main.php
@@ -6,5 +6,11 @@ return [
             'class' => 'yii\caching\FileCache',
             //'cachePath' => '@runtime/cache2',
         ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'port' => 6379,
+            'database' => 0,
+        ],
     ],
 ];
